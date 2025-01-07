59a346782e73c0dc679f01bdb7f8aaf2580e9b9e
VSchaepertoens <vschaepertoens@protonmail.com>
2025-01-07 15:49:16 +0100

    separates filename

--- scripts/prepare_samples_overview.R
+++ scripts/prepare_samples_overview.R
@@ -6,7 +6,21 @@ df <- tibble(raw_full_path = dir_ls(path = "data",regexp =  ".*\\.raw"),) %>%
            into = c("data", "filename"),
            sep = "/",
            remove = FALSE) %>%
-  mutate(sample_name = str_extract(filename, "([^_]+_[^_]+_[^_]+_[^_]+_[^_]+_[^_]+)"))
+  mutate(sample_name = str_extract(filename, "([^_]+_[^_]+_[^_]+_[^_]+_[^_]+_[^_]+)")) %>%
+  separate(filename,
+           into = c("ymd", 
+                    "initials",
+                    "product",
+                    "condition", 
+                    "biological_replicate",
+                    "timepoint", 
+                    "technical_replicate",
+                    "pngase",
+                    "acquisition_number"
+           ),
+           sep = "_",
+           remove = FALSE) %>%
+  mutate(condition_br_tp = paste(condition,biological_replicate,timepoint, sep = "_"))
 
 write_csv(x = df, 
           file = "data/samples_overview.csv")
